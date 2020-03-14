7d602ece0c4c998f141d4d21dcd0ca6002c65d64
dawei2233 <cdw32934@gmail.com>
2020-03-14 16:48:43 +0800

    submit

--- src/main/java/com/ithome/controller/IndexController.java
+++ src/main/java/com/ithome/controller/IndexController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/hello")
-    public  String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @RequestMapping("/index")
+    public  String Index(){
+        return "index";
     }
 
 }
